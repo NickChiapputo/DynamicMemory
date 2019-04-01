8b5012b1c061897781b9a2cc2ab2b5908fcedc68
NickChiapputo <chiapputo.nick@gmail.com>
2019-03-31 19:39:17 -0500

    Finds open space for allocation.

--- DynamicMemory.asm
+++ DynamicMemory.asm
@@ -4,70 +4,70 @@
 		# Based on input, memory is allocated, deallocated, or loop is restarted
 		inputLoop:
 			# Prompt for command
-			li 		$v0, 4				# Load print_string command
-			la 		$a0, CommandRequest # Give command request to user
+			li 			$v0, 4						# Load print_string command
+			la 			$a0, CommandRequest 		# Give command request to user
 			syscall
 
-			la 		$a0, Input			# Load address of input string
-			syscall						# Print input string
+			la 			$a0, Input					# Load address of input string
+			syscall									# Print input string
 
 
 			# Read command
-			li 		$v0, 8				# Load read_string command
-			la 		$a0, UserString		# Put buffer address in $a0
-			addi 	$a1, $zero, 12 		# Read a maximum of 12 characters (deallocate = 10 + '\n' + '\0')
-			syscall						# User command will be stored in $a0
-			jal removeNewLine			# Remove trailing newline from input
+			li 			$v0, 8						# Load read_string command
+			la 			$a0, UserString				# Put buffer address in $a0
+			addi 		$a1, $zero, 12 				# Read a maximum of 12 characters (deallocate = 10 + '\n' + '\0')
+			syscall									# User command will be stored in $a0
+			jal 		removeNewLine				# Remove trailing newline from input
 
 
 			# Compare input string and "allocate"
-			la 		$a1, Allocate 		# Load address of Allocate in $a1
-			jal 	strcmp
+			la 			$a1, Allocate 				# Load address of Allocate in $a1
+			jal 		strcmp
 
 
 			# Check if user string matches allocate (i.e., $v0 = 1)
-			addi 	$t0, $zero, 1
-			beq 	$v0, $t0, allocateMemory
+			addi 		$t0, $zero, 1
+			beq 		$v0, $t0, allocateMemory
 
 
 			# If user string does not match allocate, compare with "deallocate"
-			la 		$a1, Deallocate 	# Load address of Deallocate in $a1
-			jal 	strcmp
+			la 			$a1, Deallocate 			# Load address of Deallocate in $a1
+			jal 		strcmp
 
 
 			# Check if user string matches deallocate (i.e., $v0 = 1)
-			addi 	$t0, $zero, 1
-			beq 	$v0, $t0, deallocateMemory
+			addi 		$t0, $zero, 1
+			beq 		$v0, $t0, deallocateMemory
 
 
 			# If user string does not match deallocate, compare with "quit"
-			la 		$a1, Quit
-			jal		strcmp
+			la 			$a1, Quit
+			jal			strcmp
 
 
 			# Check if user string matches quit (i.e., $v0 = 1)
-			addi 	$t0, $zero, 1
-			beq 	$v0, $t0, exit
+			addi 		$t0, $zero, 1
+			beq 		$v0, $t0, exit
 
 
 			# If return value is zero, tell user the input was bad
-			bne 	$v0, $zero, inputLoop
-			li 		$v0, 4
-			la 		$a0, BadInput
+			bne 		$v0, $zero, inputLoop
+			li 			$v0, 4
+			la 			$a0, BadInput
 			syscall
 
-			j 		inputLoop 			# Repeat input loop
+			j 			inputLoop 					# Repeat input loop
 
 
 		# Terminates execution
 		exit:
 			# Tell user program is done
-			li 		$v0, 4
-			la 		$a0, Stop
+			li 			$v0, 4
+			la 			$a0, Stop
 			syscall
 
 			# Execute syscall 10 to terminate execution
-			li 		$v0, 10
+			li 			$v0, 10
 			syscall
 
 
@@ -86,42 +86,111 @@
 	# #
 	allocateMemory:
 		# Prompt user for allocation size
-		li 			$v0, 4				# Load print_string command
-		la 			$a0, SizePrompt		# Load address of SizePrompt string
+		li 				$v0, 4						# Load print_string command
+		la 				$a0, SizePrompt				# Load address of SizePrompt string
 		syscall
 
 		
 		# Get user input for allocation size
-		li 			$v0, 5				# Load read_int command
+		li 				$v0, 5						# Load read_int command
 		syscall
 
 
 		# Get number of chunks required
-		xor 		$t0, $t0, $t0 		# Set initial number of chunks to zero
-		addi 		$t1, $v0, 0			# Store allocation size in $t1 to free $v0
+		xor 			$t0, $t0, $t0 				# Set initial number of chunks to zero
+		addi 			$t1, $v0, 0					# Store allocation size in $t1 to free $v0
 
 		chunks:
-			addi 	$t1, $t1, -32		# Decrease current size by 32
-			addi 	$t0, $t0, 1			# Increment number of chunks needed
-			bgt 	$t1, $zero, chunks 	# Continue loop while current size is greater than zero
+			addi 		$t1, $t1, -32				# Decrease current size by 32
+			addi 		$t0, $t0, 1					# Increment number of chunks needed
+			bgt 		$t1, $zero, chunks 			# Continue loop while current size is greater than zero
+
+		add 			$s0, $t0, $zero 			# Save number of chunks in $s0	
 
 
 		# Search for available contiguous space
-		xor 		$t1, $t1, $t1		# Set current number of contiguous spots to zero
+		la 				$a1, ChunkList 				# Load address of the data table
+		lw 				$t0, ($a1)					# Load first value
+		xor				$t1, $t1, $t1 				# Initialize index to zero
+		addi 			$t2, $zero, 64				# Set max index value
+		xor				$t3, $t3, $t3 				# Initialize start index to zero
+		xor				$t4, $t4, $t4 				# Initialize contiguous empty memory count to zero
 
 		searchForSpace:
+			li 			$v0, 1						# Load print_int command
+			add 		$a0, $t1, $zero 			# Store index to print 
+			syscall
+
+			li 			$v0, 4						# Load print_string command
+			la 			$a0, Dash					# Load address of " - " string
+			syscall
+
+			lw 			$a0, ($a1) 					# Load current value
+			li 			$v0, 1 						# Load print_int command
+			syscall 								# Pint current chunk value
 
 
+			bne 		$a0, $zero, resetSearch		# If current value is not zero. reset counter and address start
 
-		j inputLoop 					# Restart the input loop
+			addi 		$t4, $t4, 1 				# Increment contiguous count
+			beq 		$t4, $s0, endSearchForSpace	# Break out of loop if enough contiguous spaces have been found
+			j 			skipReset 					# Avoid resetting the values
 
-	deallocateMemory:
+			resetSearch:
+			addi 		$t3, $t1, 1 				# Reset start index to the next index
+			xor 		$t4, $t4, $t4 				# Reset contiguous count
+
+			skipReset:
+
+			# Print newline
+			li 			$v0, 4
+			la 			$a0, NewLine
+			syscall
+
+			addi 		$a1, $a1, 4 				# Increment address by 4 bytes (size of integer)
+			addi 		$t1, $t1, 1 				# Increment index by 1
+
+			bne 		$t1, $t2, searchForSpace
+		endSearchForSpace:  
+
+
+		# Print 2x newline
 		li $v0, 4
-		la $a0, Deallocate
+		la $a0, NewLine
+		syscall
+		syscall
+
+		# Print start index
+		li $v0, 1
+		addi $a0, $t3, 0
+		syscall
+
+		# Print Dash
+		li $v0, 4
+		la $a0, Dash
+		syscall
+
+		# Print contiguous count
+		li $v0, 1
+		addi $a0, $t4, 0
+		syscall
+
+		# Print newline
+		li $v0, 4
+		la $a0, NewLine
 		syscall
 
 
-		j inputLoop 		# Restart the input loop
+
+		j 				inputLoop 					# Restart the input loop
+
+	deallocateMemory:
+		li 				$v0, 4
+		la 				$a0, Deallocate
+		syscall
+
+
+		j 				inputLoop 					# Restart the input loop
 
 
 	# #
@@ -141,19 +210,19 @@
 	# and replace the newline character with a null-terminator
 	# #
 	removeNewLine:
-		xor 	$t0, $a0, $zero 			# Set $t0 to string address
-		xor 	$t1, $t1, $t1				# Set counter to zero
+		xor 			$t0, $a0, $zero 				# Set $t0 to string address
+		xor 			$t1, $t1, $t1					# Set counter to zero
 
 		remove:
-			lbu 	$t2, ($t0)				# Get character in string
-			addiu 	$t0, $t0, 1				# Increment pointer
-			bne 	$t2, $zero, remove 		# Keep looping until null-terminator found
-			beq 	$a1, $a2, skip 			# Check if string has been completely read
-			addiu 	$t0, $t0, -2 			# Move back past null-terminator to newline index
-			sb 		$zero, ($t0)			# Insert null-terminator where null-terminator was
+			lbu 		$t2, ($t0)						# Get character in string
+			addiu 		$t0, $t0, 1						# Increment pointer
+			bne 		$t2, $zero, remove 				# Keep looping until null-terminator found
+			beq 		$a1, $a2, skip 					# Check if string has been completely read
+			addiu 		$t0, $t0, -2 					# Move back past null-terminator to newline index
+			sb 			$zero, ($t0)					# Insert null-terminator where null-terminator was
 
 		skip:
-			jr $ra
+			jr 			$ra
 
 
 	# #
@@ -175,52 +244,53 @@
 	#	}
 	# #
 	strcmp:
-		move $t0, $a0					# Save str1 in temp variable
-		move $t1, $a1					# Save str2 in temp variable
+		move 			$t0, $a0					# Save str1 in temp variable
+		move 			$t1, $a1					# Save str2 in temp variable
 
 		# Loop until end of str1 is found (equal result) or current chars are not equal (not equal result)
 		loop:
-			lbu		$t2, ($t0)			# Get next character from str1
-			lbu		$t3, ($t1)			# Get next character from str2
+			lbu			$t2, ($t0)					# Get next character from str1
+			lbu			$t3, ($t1)					# Get next character from str2
 
-			addi 	$t0, $t0, 1 		# Increment str1 pointer
-			addi 	$t1, $t1, 1 		# Increment str2 pointer
+			addi 		$t0, $t0, 1 				# Increment str1 pointer
+			addi 		$t1, $t1, 1 				# Increment str2 pointer
 
-			beq 	$t2, $zero, equal 	# If at end of string, strings are equal
-			bne 	$t2, $t3, notEqual	# Compare characters from both strings
+			beq 		$t2, $zero, equal 			# If at end of string, strings are equal
+			bne 		$t2, $t3, notEqual			# Compare characters from both strings
 
-			j loop						# Restart the loop
+			j 			loop						# Restart the loop
 
 
 		# str1 != str2
 		notEqual:
-			move 	$v0, $zero			# Set return value to 0
-			jr $ra
+			move 		$v0, $zero					# Set return value to 0
+			jr 			$ra
 
 		# str1 == str2
 		equal:
-			addi 	$v0, $zero, 1		# Set return value to 1
-			jr $ra
+			addi 		$v0, $zero, 1				# Set return value to 1
+			jr 			$ra
 
 .data
-	Quit:			.asciiz "quit"
-	Stop:			.asciiz "End of Program."
-	Input: 			.asciiz ">> "	
-	NewLine:		.asciiz "\n"
-	Allocate: 		.asciiz "allocate"
-	NumChunks:		.asciiz "Number of chunks needed: "
-	BadInput: 		.asciiz "Invalid command.\n\n"
-	Deallocate: 	.asciiz "deallocate"
-	NamePrompt: 	.asciiz "Variable Name\n>> "
-	SizePrompt: 	.asciiz "Size to allocate\n>> "
-	CommandRequest: .asciiz "Menu:\n\tAllocate\n\tDeallocate\n\tQuit\n"
+	Dash:				.asciiz " - "
+	Quit:				.asciiz "quit"
+	Stop:				.asciiz "End of Program."
+	Input: 				.asciiz ">> "	
+	NewLine:			.asciiz "\n"
+	Allocate: 			.asciiz "allocate"
+	NumChunks:			.asciiz "Number of chunks needed: "
+	BadInput: 			.asciiz "Invalid command.\n\n"
+	Deallocate: 		.asciiz "deallocate"
+	NamePrompt: 		.asciiz "Variable Name\n>> "
+	SizePrompt: 		.asciiz "Size to allocate\n>> "
+	CommandRequest: 	.asciiz "Menu:\n\tAllocate\n\tDeallocate\n\tQuit\n"
 
 	# Holds the user command string and variable name input
-	UserString: 	.space 11
+	UserString: 		.space 11
 
 	# Holds user-defined variable names (64 names x 21 max size = 1344 bytes)
 	# Max size of 21 = 20 characters + '\0'
-	VarNames:		.space 1344
+	VarNames:			.space 1344
 
 	# This shows the data table for all 64 chunks of data
-	ChunkList: 		.word	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+	ChunkList: 			.word	0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 64
